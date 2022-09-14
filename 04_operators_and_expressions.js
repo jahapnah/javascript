2a98d48518457f26ec956037f84c38488ab874f4
Rishi Kant <110294979+jahapnah@users.noreply.github.com>
2022-09-15 00:51:37 +0530

    Add files via upload

---
+++ 04_operators_and_expressions.js
@@ -0,0 +1,62 @@
+
+console.log("operators in js")
+
+//ARITHMETIC OPERATORS
+let a = 5;
+let b = 2;
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+console.log("a*b =",a*b);
+console.log("a/b =",a/b);
+console.log("a**b =",a**b);
+console.log("a%b =",a%b);
+
+
+console.log("a++ =",a++);  //value then increment    
+console.log("++a =",++a);  //increment then value
+console.log("a-- =",a--);  //value then decrement
+console.log("--a =",--a);  //decrement then value
+// 5, 7, 7, 5,
+
+
+//ASSIGNMENT OPERATORS
+
+let c = 7; 
+c+= 2;
+console.log(c);
+c-= 3;
+console.log(c);
+c*= 2;
+console.log(c);
+c/= 2;
+console.log(c);
+c%= 4;
+console.log(c);
+c**= 4;
+console.log(c);
+// 9, 6, 12, 6, 2, 16
+
+//COMPARISON OPERATORS
+
+let x = 10
+let y  = 5
+console.log("x == y is ", x==y);
+console.log("x != y is ", x!=y);
+
+let p = "5";
+let q = 5;
+console.log("p===q is ", p===q);
+console.log("p!==q is ", p!==q);
+
+//LOGICAL OPERATORS
+
+let j = 3
+let k = 5
+console.log(j<k && j==3)
+console.log(j>k || k==5)
+console.log(!true)
+console.log(!false)
+
+
+
+
