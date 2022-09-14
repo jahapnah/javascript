74fe29a7092414b56755ef7e2a195c047db46d7c
Rishi Kant <110294979+jahapnah@users.noreply.github.com>
2022-09-14 22:07:38 +0530

    Add files via upload

---
+++ 02_data_types.js
@@ -0,0 +1,37 @@
+// VARIABLES
+// var, let, const
+
+var name = 'rishi';
+var channel;
+channel='janpathop';
+var marks = 34;
+var city = 'Delhi'
+console.log(name, channel, city, marks);
+
+// Rules for creating javascript variables
+// 1. Cannot start with numbers 
+// 2.Can start with numbers , $ , _ and letters
+// 3. Javascript variabless are case sensitive
+
+const books = 'Making India Awesome';
+// books ='Macroeconomics'  
+// changing the value of variables is not applicable in const
+console.log(books);
+
+// Block level variable 
+
+{
+    let city = 'Dehradun';
+    console.log(city)
+}
+
+console.log(city)
+
+
+/* Most common programming case types -
+
+1. camelCase
+2. kebab-case
+3.snake_case
+4. PascalCase
+*/
